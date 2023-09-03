f8784a506dbe453aa5142895980bebe93e03b26d
Tejiri Emoghene-Ijatomi <steveijatomi@gmail.com>
2023-09-04 00:43:38 +0100

    CHANGES

--- database/migrations/2023_08_10_190000_create_users_table.php
+++ database/migrations/2023_08_10_190000_create_users_table.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Schema::dropIfExists('users');
-        Schema::create('users', function (Blueprint $table) {
+        Schema::dropIfExists('users');
+        Schema::create('users', function (Blueprint $table) {    
             $table->id();
             $table->string('username');
             $table->string('firstName',255);
