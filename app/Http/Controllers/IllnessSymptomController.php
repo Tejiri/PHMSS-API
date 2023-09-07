d53cc03a3e051c3f853223ad1eb62c9a7b696922
Tejiri Emoghene-Ijatomi <steveijatomi@gmail.com>
2023-09-08 00:09:48 +0100

    CHANGES

--- app/Http/Controllers/IllnessSymptomController.php
+++ app/Http/Controllers/IllnessSymptomController.php
@@ -6,26 +6,42 @@ use App\Models\Illness;
 use App\Models\Symptom;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class IllnessSymptomController extends Controller
 {
     //
     function findIllnessWithSymptoms(Request $request)
     {
+
         try {
-            return response()->json([
-                "symptoms" => $request->symptoms,
-            ], 200);
-            $possibleIllnesses = Illness::with('symptoms')->whereHas('symptoms', function ($query) use ($request) {
-                $query->whereIn('name', $request->symptoms);
-            })->get();
+            $symptoms = $request->input('symptoms'); // Use input() method to get the symptoms
     
+            // Log symptoms for debugging
+            Log::info('Symptoms: ' . json_encode($symptoms));
+    
+            // Return symptoms in response for debugging
             return response()->json([
-                "possibleIllnesses" => $possibleIllnesses,
+                "symptoms" => $symptoms,
             ], 200);
+    
         } catch (\Throwable $th) {
-            return $th;
+            return response()->json(['error' => $th->getMessage()], 500);
         }
+        // try {
+        //     return response()->json([
+        //         "symptoms" => $request->symptoms,
+        //     ], 200);
+        //     $possibleIllnesses = Illness::with('symptoms')->whereHas('symptoms', function ($query) use ($request) {
+        //         $query->whereIn('name', $request->symptoms);
+        //     })->get();
+    
+        //     return response()->json([
+        //         "possibleIllnesses" => $possibleIllnesses,
+        //     ], 200);
+        // } catch (\Throwable $th) {
+        //     return $th;
+        // }
        
     }
 
